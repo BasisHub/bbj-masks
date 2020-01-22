2bb1c465930981fb1b82f93b40dd21c45fcfe669
Hyyan Abo Fakher <hyyanaf@gmail.com>
2020-01-22 10:32:44 +0100

    chore: add webpack bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
-const webpack = require("webpack");
+const webpack = require('webpack')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const path = require("path");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
+const path = require('path')
 
 module.exports = {
   entry: {
-    "bbj-masks": "./src/index.js",
-    "bbj-masks.min": "./src/index.js"
+    'bbj-masks': './src/index.js',
+    'bbj-masks.min': './src/index.js'
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-    library: ["BBj", "Masks"],
-    libraryTarget: "umd"
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    library: ['BBj', 'Masks'],
+    libraryTarget: 'umd'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: [['@babel/preset-env',{ "modules":false }]]
+          presets: [['@babel/preset-env', { modules: false }]]
         }
       }
     ]
@@ -29,9 +31,13 @@ module.exports = {
   plugins: [
     new UglifyJsPlugin({
       include: /\.min\.js$/
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false
     })
   ],
   watchOptions: {
     ignored: /node_modules/
   }
-};
+}
